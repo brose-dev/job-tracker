fd77e5a0f0bf3d9e2a67a483e9ff92c654156359
brose-dev <rosebritania102@gmail.com>
2026-06-22 16:12:45 -0400

    chore: remove files that should have been gitignored

--- backend/jobs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Job
-
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'status', 'fit_score', 'remote_type', 'date_added')
-    list_filter = ('status', 'remote_type')
-    search_fields = ('company', 'title')
-    
